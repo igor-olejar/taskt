99546d9df57561e65c3df6310a00a0a4bd101333
Igor Olejar <igor.olejar@gmail.com>
2014-08-29 11:36:24 +0100

    Password reminder migration and database dump

--- dbdump/taskt.sql
+++ dbdump/taskt.sql
@@ -1,35 +1,28 @@
--- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.5.38, for debian-linux-gnu (i686)
 --
--- Host: localhost
--- Generation Time: Jun 27, 2014 at 10:53 AM
--- Server version: 5.5.37
--- PHP Version: 5.5.13-2+deb.sury.org~precise+1
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: taskt
+-- ------------------------------------------------------
+-- Server version	5.5.38-0ubuntu0.12.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `taskt`
---
-CREATE DATABASE IF NOT EXISTS `taskt`;
-
-USE `taskt`;
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `checkins`
 --
 
-CREATE TABLE IF NOT EXISTS `checkins` (
+DROP TABLE IF EXISTS `checkins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `checkins` (
   `start_date` datetime DEFAULT NULL,
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -38,22 +31,27 @@ CREATE TABLE IF NOT EXISTS `checkins` (
   `start` int(11) NOT NULL,
   `end` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `checkins`
 --
 
-INSERT INTO `checkins` (`start_date`, `id`, `created_at`, `updated_at`, `task_id`, `start`, `end`) VALUES
-(NULL, 1, '2014-06-30 09:14:20', '2014-06-30 09:14:20', 1, 1404119660, 1404123260);
-
--- --------------------------------------------------------
+LOCK TABLES `checkins` WRITE;
+/*!40000 ALTER TABLE `checkins` DISABLE KEYS */;
+INSERT INTO `checkins` VALUES (NULL,1,'2014-06-30 09:14:20','2014-06-30 09:14:20',1,1404119660,1404123260);
+/*!40000 ALTER TABLE `checkins` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `clients`
 --
 
-CREATE TABLE IF NOT EXISTS `clients` (
+DROP TABLE IF EXISTS `clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clients` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -61,47 +59,128 @@ CREATE TABLE IF NOT EXISTS `clients` (
   `description` text COLLATE utf8_unicode_ci,
   `website` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `clients`
 --
 
-INSERT INTO `clients` (`id`, `created_at`, `updated_at`, `name`, `description`, `website`) VALUES
-(1, '2014-06-30 09:14:20', '2014-06-30 09:14:20', 'Happy Company', 'We are very happy', 'http://www.happy.com'),
-(2, '2014-06-30 09:14:20', '2014-06-30 09:14:20', 'Sad Company', 'We are very sad', 'http://www.sad.com'),
-(3, '2014-06-30 09:14:20', '2014-06-30 09:14:20', 'Third Company', '', '');
+LOCK TABLES `clients` WRITE;
+/*!40000 ALTER TABLE `clients` DISABLE KEYS */;
+INSERT INTO `clients` VALUES (1,'2014-06-30 09:14:20','2014-06-30 09:14:20','Happy Company','We are very happy','http://www.happy.com'),(2,'2014-06-30 09:14:20','2014-06-30 09:14:20','Sad Company','We are very sad','http://www.sad.com'),(3,'2014-06-30 09:14:20','2014-06-30 09:14:20','Third Company','','');
+/*!40000 ALTER TABLE `clients` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `dashboards`
+--
+
+DROP TABLE IF EXISTS `dashboards`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dashboards` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `tasks` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dashboards`
+--
+
+LOCK TABLES `dashboards` WRITE;
+/*!40000 ALTER TABLE `dashboards` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dashboards` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `migrations`
 --
 
-CREATE TABLE IF NOT EXISTS `migrations` (
+DROP TABLE IF EXISTS `migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `migrations` (
   `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `migrations`
 --
 
-INSERT INTO `migrations` (`migration`, `batch`) VALUES
-('2014_06_27_133240_create_users_table', 1),
-('2014_06_27_133500_create_projects_table', 1),
-('2014_06_27_133602_create_tasks_table', 1),
-('2014_06_27_133705_create_clients_table', 1),
-('2014_06_27_133806_create_checkins_table', 1),
-('2014_06_27_143348_create_project_user_table', 1);
+LOCK TABLES `migrations` WRITE;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+INSERT INTO `migrations` VALUES ('2014_06_27_133240_create_users_table',1),('2014_06_27_133500_create_projects_table',1),('2014_06_27_133602_create_tasks_table',1),('2014_06_27_133705_create_clients_table',1),('2014_06_27_133806_create_checkins_table',1),('2014_06_27_143348_create_project_user_table',1),('2014_06_30_102700_create_dashboards_table',2),('2014_08_29_103048_create_password_reminders_table',2);
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `password_reminders`
+--
+
+DROP TABLE IF EXISTS `password_reminders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `password_reminders` (
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  KEY `password_reminders_email_index` (`email`),
+  KEY `password_reminders_token_index` (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `password_reminders`
+--
+
+LOCK TABLES `password_reminders` WRITE;
+/*!40000 ALTER TABLE `password_reminders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `password_reminders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `project_user`
+--
+
+DROP TABLE IF EXISTS `project_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `project_user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `project_id` int(10) unsigned NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `project_user`
+--
+
+LOCK TABLES `project_user` WRITE;
+/*!40000 ALTER TABLE `project_user` DISABLE KEYS */;
+INSERT INTO `project_user` VALUES (1,2,1,'2014-06-30 09:14:20','2014-06-30 09:14:20'),(2,2,2,'2014-06-30 09:14:20','2014-06-30 09:14:20');
+/*!40000 ALTER TABLE `project_user` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `projects`
 --
 
-CREATE TABLE IF NOT EXISTS `projects` (
+DROP TABLE IF EXISTS `projects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `projects` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -113,46 +192,27 @@ CREATE TABLE IF NOT EXISTS `projects` (
   `rate` int(11) DEFAULT NULL,
   `roundup` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `projects`
 --
 
-INSERT INTO `projects` (`id`, `created_at`, `updated_at`, `name`, `description`, `client_id`, `start_date`, `end_date`, `rate`, `roundup`) VALUES
-(1, '2014-06-30 09:14:20', '2014-06-30 09:14:20', 'Project1', 'I am the first project ever', 1, '2014-06-12 00:00:00', '2014-07-12 00:00:00', NULL, NULL),
-(2, '2014-06-30 09:14:20', '2014-06-30 09:14:20', 'Project2', 'I am the second project', 1, '2014-06-10 00:00:00', '2015-07-10 00:00:00', NULL, NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `project_user`
---
-
-CREATE TABLE IF NOT EXISTS `project_user` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned NOT NULL,
-  `project_id` int(10) unsigned NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `project_user`
---
-
-INSERT INTO `project_user` (`id`, `user_id`, `project_id`, `created_at`, `updated_at`) VALUES
-(1, 2, 1, '2014-06-30 09:14:20', '2014-06-30 09:14:20'),
-(2, 2, 2, '2014-06-30 09:14:20', '2014-06-30 09:14:20');
-
--- --------------------------------------------------------
+LOCK TABLES `projects` WRITE;
+/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
+INSERT INTO `projects` VALUES (1,'2014-06-30 09:14:20','2014-06-30 09:14:20','Project1','I am the first project ever',1,'2014-06-12 00:00:00','2014-07-12 00:00:00',NULL,NULL),(2,'2014-06-30 09:14:20','2014-06-30 09:14:20','Project2','I am the second project',1,'2014-06-10 00:00:00','2015-07-10 00:00:00',NULL,NULL);
+/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tasks`
 --
 
-CREATE TABLE IF NOT EXISTS `tasks` (
+DROP TABLE IF EXISTS `tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tasks` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -163,24 +223,27 @@ CREATE TABLE IF NOT EXISTS `tasks` (
   `end_date` datetime DEFAULT NULL,
   `status` varchar(20) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'todo',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `tasks`
 --
 
-INSERT INTO `tasks` (`id`, `created_at`, `updated_at`, `description`, `user_id`, `project_id`, `start_date`, `end_date`, `status`) VALUES
-(1, '2014-06-30 09:14:20', '2014-06-30 09:14:20', 'I am task one', 2, 1, '2014-06-27 00:00:00', '0000-00-00 00:00:00', 'in progress'),
-(2, '2014-06-30 09:14:20', '2014-06-30 09:14:20', 'I am task two', 2, 1, '2014-06-27 00:00:00', '2014-06-27 00:00:00', 'completed'),
-(3, '2014-06-30 09:14:20', '2014-06-30 09:14:20', 'I am task one', 2, 2, NULL, NULL, 'todo');
-
--- --------------------------------------------------------
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+INSERT INTO `tasks` VALUES (1,'2014-06-30 09:14:20','2014-06-30 09:14:20','I am task one',2,1,'2014-06-27 00:00:00','0000-00-00 00:00:00','in progress'),(2,'2014-06-30 09:14:20','2014-06-30 09:14:20','I am task two',2,1,'2014-06-27 00:00:00','2014-06-27 00:00:00','completed'),(3,'2014-06-30 09:14:20','2014-06-30 09:14:20','I am task one',2,2,NULL,NULL,'todo');
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -193,11 +256,26 @@ CREATE TABLE IF NOT EXISTS `users` (
   `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `created_at`, `updated_at`, `username`, `password`, `firstname`, `lastname`, `email`, `company`, `remember_token`) VALUES
-(1, '2014-06-30 09:14:20', '2014-07-17 07:38:12', 'igor', '$2y$10$GRwfCQ38nNz/HfAB0g7/lOHrBFTRBZM3bzSp5x3sG7dE64.tbTuOq', 'Igor', 'Olejar', 'igor.olejar@gmail.com', 'TeknoStan Limited', 'ykavtNfCMU5zjMDL4Rrk93B2bWHTmCP3WdHLAh74PxRKXpIAXxjpind7kz9m');
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'2014-06-30 09:14:20','2014-08-29 09:10:51','igor','$2y$10$GRwfCQ38nNz/HfAB0g7/lOHrBFTRBZM3bzSp5x3sG7dE64.tbTuOq','Igor','Olejar','igor.olejar@gmail.com','TeknoStan Limited','DDYxl99QrdaYdpohW6hBXLeEyZKogPT6VU3HzwbWBcl6ezZjPaCvYutQOjlj');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-08-29 11:35:10
